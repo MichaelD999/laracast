545257abba6a0321c12457ee9072b2e623545591
Michael Diaz <michaeldiaz@Michaels-MacBook-Pro.local>
2021-03-03 12:05:21 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+$task = [
+'title' => 'Finish homework',
+'due' => 'today',
+'assigned_to' => 'jeffrey',
+'completed' => true
+];
+
+
+require 'index.view.php';
---
+++ index.view.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Document</title>
+	<style>
+		header {
+			background: #e3e3e3;
+			padding: 2em;
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<h1>Task for the day</h1>
+	<ul>
+		<li>
+			<strong>Name:</strong> <?= $task['title']; ?>
+		</li>
+
+		<li>
+			<strong>Due Date:</strong> <?= $task['due']; ?>
+		</li>
+
+		<li>
+			<strong>Personal Responsible:</strong> <?= $task['assigned_to']; ?>
+		</li>
+		<li>
+			<strong>Status:</strong> <?= $task['completed']? 'Complete' : 'Incomplete'; ?>
+		</li>
+	</ul>
+</body>
+</html>
