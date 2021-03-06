68b7c63ceb250ac7654ed3f415c8f1c4d415578d
Michael Diaz <michaeldiaz@Michaels-MacBook-Pro.local>
2021-03-06 19:06:22 +0100

    password

--- index.php
+++ index.php
@@ -3,15 +3,10 @@
 require 'bootstrap.php';
 
 
-require 'Task.php';
-
-$pdo = Connection::make();
-
-$query = new QueryBuilder($pdo);
 
 
 $tasks = $query->selectALL('todos');
 
-die(var_dump($tasks));
+
 
 require 'index.view.php';
